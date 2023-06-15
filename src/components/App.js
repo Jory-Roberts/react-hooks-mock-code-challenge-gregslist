10bf813c78c922148aaf183436b14b867fc6ccaf
Jory Roberts <robertsjory13@gmail.com>
2023-06-15 02:23:56 -0500

    logs response

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,26 @@
-import React from "react";
-import Header from "./Header";
-import ListingsContainer from "./ListingsContainer";
+import React, { useEffect, useState } from 'react';
+import Header from './Header';
+import ListingsContainer from './ListingsContainer';
+
+const API = ' http://localhost:6001/listings ';
 
 function App() {
+  const [listings, setListings] = useState([]);
+
+  const fetchListings = async () => {
+    const response = await fetch(API).then((response) => response.json());
+    console.log(response);
+    setListings(response);
+  };
+
+  useEffect(() => {
+    fetchListings();
+  }, []);
+
   return (
-    <div className="app">
+    <div className='app'>
       <Header />
-      <ListingsContainer />
+      <ListingsContainer listings={listings} />
     </div>
   );
 }
